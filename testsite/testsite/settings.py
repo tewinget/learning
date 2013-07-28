8e99318dfe83b08e34f3d684755f4432ba492643
pshatara <pshatara@gmail.com>
2013-07-27 18:57:18 -0700

    updated settings 1

--- testsite/testsite/settings.py
+++ testsite/testsite/settings.py
@@ -4,7 +4,8 @@ DEBUG = True
 TEMPLATE_DEBUG = DEBUG
 
 ADMINS = (
-    # ('Your Name', 'your_email@example.com'),
+    ('Peter Shatara', 'pshatara@gmail.com'),
+	('Tom Winget', 'tewinget@gmail.com'),
 )
 
 MANAGERS = ADMINS
